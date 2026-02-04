e6d31fce3cb29f3a8385084364ccd2a44565fc97
Mussie Tarekegn Baye <mussie.tarekegn@aastustudent.edu.et>
2026-02-04 09:26:17 +0300

    Create Power - Mod Power.py

---
+++ Power - Mod Power.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a=int(input())
+b=int(input())
+m=int(input())
+
+
+mod=(a**b)%m
+p=a**b
+print(p)
+if b>0:
+    
+    print(mod)
