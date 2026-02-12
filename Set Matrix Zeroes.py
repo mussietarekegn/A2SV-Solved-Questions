e83ffad7f1d0ce78a85500c1d73b6219c3cd3db2
Mussie Tarekegn Baye <mussie.tarekegn@aastustudent.edu.et>
2026-02-12 15:20:43 +0300

    Create Set Matrix Zeroes.py

---
+++ Set Matrix Zeroes.py
@@ -0,0 +1,19 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        zero_row=set()
+        zero_col=set()
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j]==0:
+                    zero_row.add(i)
+                    zero_col.add(j)
+        
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if i in zero_row or j in zero_col:
+                    matrix[i][j]=0
+        
