7b1ccb9c106da91a7daae9827d8d6c251fc65d6e
Mussie Tarekegn Baye <mussie.tarekegn@aastustudent.edu.et>
2026-03-10 16:11:51 +0300

    Add solution for Next Greater Element I

---
+++ leetcode/0496-Next-Greater-Element-I/0496-Next-Greater-Element-I.py
@@ -0,0 +1,20 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        mapp={}
+        stack=[]
+
+        for i in range(len(nums2)):
+            
+            while stack and stack[-1]<nums2[i]:
+                prev=stack.pop()
+                mapp[prev]=nums2[i]
+
+            stack.append(nums2[i])
+        res=[]
+
+        while stack:
+            mapp[stack.pop()]=-1
+        for i in range(len(nums1)):
+            res.append(mapp[nums1[i]])
+        
+        return res
