34272504a9b08245c8f4b6fbb52a7814ba623e08
Mussie Tarekegn Baye <mussie.tarekegn@aastustudent.edu.et>
2026-03-17 14:33:55 +0300

    Add solution for Count Good Numbers

---
+++ leetcode/1922-count-good-numbers/1922-count-good-numbers.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:
+
+        def helper(x,n):
+            res=1
+            while n>0:
+                if n%2!=0:
+                    res=(res*x)%(10**9+7)
+                n=n//2
+                x=(x*x)%(10**9+7)
+            return res
+        
+        e=ceil(n/2)
+        p=n//2
+
+        return (helper(5,e)*helper(4,p))%(10**9+7)
+                
