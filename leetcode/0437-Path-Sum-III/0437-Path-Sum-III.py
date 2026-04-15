03f3f04bcc1f70407bd909d01a2bc6aca8973aea
Mussie Tarekegn Baye <bayemussie@gmail.com>
2026-04-15 19:58:15 +0300

    Add solution for Path Sum III

---
+++ leetcode/0437-Path-Sum-III/0437-Path-Sum-III.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root, targetSum):
+        prefix = {0: 1}
+        
+        def dfs(node, curr_sum):
+            if not node:
+                return 0
+            
+            curr_sum += node.val
+  
+            count = prefix.get(curr_sum - targetSum, 0)
+
+            prefix[curr_sum] = prefix.get(curr_sum, 0) + 1
+            
+            count += dfs(node.left, curr_sum)
+            count += dfs(node.right, curr_sum)
+    
+            prefix[curr_sum] -= 1
+            
+            return count
+        
+        return dfs(root, 0)
