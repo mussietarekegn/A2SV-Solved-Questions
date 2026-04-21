ea627f0e458a9c36fb465bcf29ea28749e131f21
Mussie Tarekegn Baye <bayemussie@gmail.com>
2026-04-21 19:04:32 +0300

    Add solution for Surrounded Regions

---
+++ leetcode/0130-Surrounded-Regions/0130-Surrounded-Regions.py
@@ -0,0 +1,35 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        
+        row=len(board)
+        col=len(board[0])
+
+        def check(r,c):
+            if r>=row or r<0 or c<0 or c>=col or board[r][c]!="O":
+                return
+            
+            board[r][c]="M"
+            check(r+1,c)
+            check(r-1,c)
+            check(r,c+1)
+            check(r,c-1)
+        
+        
+        for i in range(row):
+            check(i,0)
+            check(i,col-1)
+        
+        for i in range(col):
+            check(0,i)
+            check(row-1,i)
+        
+        for i in range(row):
+            for j in range(col):
+
+                if board[i][j]=="O":
+                    board[i][j]="X"
+                elif board[i][j]=="M":
+                    board[i][j]="O"
