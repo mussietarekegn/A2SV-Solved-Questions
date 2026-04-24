246a8bb526f01d2929e233d5e7eabedf574f68da
Mussie Tarekegn Baye <bayemussie@gmail.com>
2026-04-24 16:57:18 +0300

    Add solution for Employee Importance

---
+++ leetcode/0690-Employee-Importance/0690-Employee-Importance.py
@@ -0,0 +1,23 @@
+"""
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
+
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+        emp_map = {emp.id:emp for emp in employees}
+
+        def dfs(emp_id):
+            emp = emp_map[emp_id]
+            summ = emp.importance
+            
+            for sub in emp.subordinates:
+                summ+= dfs(sub)
+            
+            return summ
+
+        return dfs(id)
